02544bd1b5f9c4b45a8619bdf4cef8cced96bc97
0x152a <62497850+0x152a@users.noreply.github.com>
2023-06-17 15:35:49 +0800

    ~ Empty last tag causes error

--- check.sh
+++ check.sh
@@ -8,19 +8,16 @@ set_env() {
 GITHUB_API_LATEST="https://api.github.com/repos/gedoor/legado/releases/latest"
 
 set_env WORKSPACE $GITHUB_WORKSPACE
-LatestTag=$(curl -s $GITHUB_API_LATEST | jq .tag_name -r)
-LatestCheck=$(date -u -d"+8 hour" "+%Y-%m-%d %H:%M:%S")
 LastTag=$(cat $WORKSPACE/.lastcheck | sed -n 1p)
-echo LastTag is $LastTag
+set_env APP_LATEST_TAG $(curl -s $GITHUB_API_LATEST | jq .tag_name -r)
+set_env APP_LATEST_CHECK $(date -u -d"+8 hour" "+%Y-%m-%d %H:%M:%S")
+set_env OUTPUT_NAME_SUFFIX $OUTPUT_NAME_SUFFIX-$LatestTag
+echo LastTag is $LastTag, now is $APP_LATEST_TAG
 
 version_gt() { test "$(echo "$@" | tr " " "\n" | sort -V | head -n 1)" != "$1"; }
-if version_gt $LatestTag $LastTag; then
+if [ -z "$LastTag" -o version_gt $APP_LATEST_TAG $LastTag]; then
     set_env HAS_NEWER_VERSION "TRUE"
-    echo Found newer version: $LatestTag
+    echo Found newer version: $APP_LATEST_TAG
 else
     set_env HAS_NEWER_VERSION "FALSE"
 fi
-
-set_env APP_LATEST_TAG $LatestTag
-set_env APP_LATEST_CHECK $LatestCheck
-set_env OUTPUT_NAME_SUFFIX $OUTPUT_NAME_SUFFIX-$LatestTag
