fd4693f1fc40bdfd9409aedb8a397dd26263e529
0x152a <62497850+0x152a@users.noreply.github.com>
2023-06-17 14:31:15 +0800

    ~ check.sh do not output latest tag when debugging

--- check.sh
+++ check.sh
@@ -16,10 +16,11 @@ echo LastTag is $LastTag
 version_gt() { test "$(echo "$@" | tr " " "\n" | sort -V | head -n 1)" != "$1"; }
 if version_gt $LatestTag $LastTag; then
     set_env HAS_NEWER_VERSION "TRUE"
-    set_env APP_LATEST_TAG $LatestTag
-    set_env APP_LATEST_CHECK $LatestCheck
-    set_env OUTPUT_NAME_SUFFIX $OUTPUT_NAME_SUFFIX-$LatestTag
     echo Found newer version: $LatestTag
 else
     set_env HAS_NEWER_VERSION "FALSE"
 fi
+
+set_env APP_LATEST_TAG $LatestTag
+set_env APP_LATEST_CHECK $LatestCheck
+set_env OUTPUT_NAME_SUFFIX $OUTPUT_NAME_SUFFIX-$LatestTag
